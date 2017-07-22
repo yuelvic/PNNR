c65b8c026dce1c6b17fef0604919f84479ba0302
Eltos Ramos <eltos.ramos@xurpas.com>
2017-07-22 13:46:22 +0800

    Added User info

--- app/src/main/java/org/bitbucket/globehacks/views/fragments/ProfileFragment.java
+++ app/src/main/java/org/bitbucket/globehacks/views/fragments/ProfileFragment.java
@@ -6,18 +6,24 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.hannesdorfmann.mosby.mvp.MvpFragment;
+import com.shawnlin.preferencesmanager.PreferencesManager;
 
 import org.bitbucket.globehacks.GlobeHack;
 import org.bitbucket.globehacks.R;
+import org.bitbucket.globehacks.models.User;
 import org.bitbucket.globehacks.presenters.ProfilePresenter;
 import org.bitbucket.globehacks.services.ApiService;
+import org.bitbucket.globehacks.utils.Keys;
 import org.bitbucket.globehacks.views.interfaces.ProfileView;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Emmanuel Victor Garcia on 7/21/17.
@@ -26,6 +32,11 @@ import butterknife.ButterKnife;
 public class ProfileFragment extends MvpFragment<ProfileView, ProfilePresenter> implements ProfileView {
 
     private static final String TAG = ProfileFragment.class.getSimpleName();
+
+    @BindView(R.id.civ_profile_image) CircleImageView civProfileImage;
+    @BindView(R.id.tv_fullname) TextView tvFullName;
+    @BindView(R.id.tv_number) TextView tvNumber;
+    @BindView(R.id.tv_user_type) TextView tvUserType;
 
     @Inject ApiService apiService;
 
@@ -45,6 +56,15 @@ public class ProfileFragment extends MvpFragment<ProfileView, ProfilePresenter> 
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
         ((GlobeHack) getActivity().getApplication()).getEntityComponent().inject(this);
+
+        initialize();
+    }
+
+    private void initialize() {
+//        civProfileImage.setImageResource();
+        tvFullName.setText(getUser().getFirstname() + " " + getUser().getLastname());
+        tvNumber.setText(getUser().getMobile());
+        tvUserType.setText(getUser().getToken());
     }
 
     @NonNull
@@ -66,5 +86,10 @@ public class ProfileFragment extends MvpFragment<ProfileView, ProfilePresenter> 
     @Override
     public ApiService getApiService() {
         return apiService;
+    }
+
+    @Override
+    public User getUser() {
+        return PreferencesManager.getObject(Keys.USER, User.class);
     }
 }
